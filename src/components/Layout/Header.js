7ee5d077729ecb6bb9ba799fd9c801232d9449fc
Andykofi <andykof2@gmail.com>
2021-05-16 12:37:36 +0100

    reverse changes

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -71,7 +71,7 @@ class Header extends Component {
             <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
                 <div className="container">
                     <Link className="navbar-brand" to="/">
-                        Graz Diaspora Record Keeper, test
+                        Graz Diaspora Record Keeper
                     </Link>
                     <button
                         className="navbar-toggler"
