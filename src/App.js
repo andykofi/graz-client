8350b7a88d3f3abbfc0af8b7fa68b91b7338d852
Andykofi <andykofi2@gmail.com>
2021-05-24 12:42:01 +0100

    saving App changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from "react-dom";
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
